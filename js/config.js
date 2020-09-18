2e504122214139b990e2e92c39b05140ecc30290
Anish <6275anioshroy@gmail.com>
2020-09-18 22:35:49 +0530

    First Commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAbD2Og9ah0bP3WMiWGI2a5rHxfRYkluTA",
+    authDomain: "bingo-c34dd.firebaseapp.com",
+    databaseURL: "https://bingo-c34dd.firebaseio.com",
+    projectId: "bingo-c34dd",
+    storageBucket: "bingo-c34dd.appspot.com",
+    messagingSenderId: "743504540097",
+    appId: "1:743504540097:web:6cae8ac7da2eaadf73bf40"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
